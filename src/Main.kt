b35b9fd3fe9252e95ae943007c910e05d4c9a022
medevelopervivek <vivek.official12@gmail.com>
2024-08-11 07:07:38 +0545

    runBlocking

--- src/Main.kt
+++ src/Main.kt
@@ -1,3 +1,11 @@
+/*
+println() is a synchronous call because the task of printing the text to the output is completed before execution
+can move to the next line of code. Because each function call in main() is synchronous, the entire main()
+function is synchronous. Whether a function is synchronous or asynchronous is determined by the parts that
+it's composed of. A synchronous function returns only when its task is fully complete. So after the last print
+statement in main() is executed, all work is done. The main() function returns and the program ends.
+ */
 fun main() {
-    println("Hello World!")
+    println("Weather forecast")
+    println("Sunny")
 }
---
+++ src/delay.kt
@@ -0,0 +1,9 @@
+import kotlinx.coroutines.*
+
+fun main() {
+    runBlocking {
+        println("Weather forecast")
+        delay(1000)
+        println("Sunny")
+    }
+}
